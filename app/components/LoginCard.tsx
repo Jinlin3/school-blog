4f32fa7cd0d0fce02c4f214a2486cd0e82d1bfb7
Jack Lin <hellojjlin@gmail.com>
2024-12-27 07:48:36 -0500

    set up google authentication with oauth

--- app/components/LoginCard.tsx
+++ app/components/LoginCard.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { auth, signIn } from "@/auth"
 
-const LoginCard = () => {
+const LoginCard = async () => {
+  const session = await auth();
   return (
     <div className="flex items-center justify-center mx-auto h-[50vh] w-[40vh] bg-white border border-red-500 rounded-2xl">
-      <button className="bg-blue-400 text-black p-3 rounded-lg">Login with Google</button>
+      {session && session?.user ? (
+        <span className="text-black text-center">Thanks for logging in {session?.user?.name}</span>
+      ) : (
+        <>
+          Please log in.
+        </>
+      )}
+      <form action={async () => {
+        "use server"
+        await signIn("google")
+      }}>
+        <button type="submit" className="bg-blue-400 text-black p-3 rounded-lg">Login with Google</button>
+      </form>
     </div>
   )
 }
