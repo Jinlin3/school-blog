c872ff27c3f7028160a1cc24d2da36f05d216fa2
Jack Lin <hellojjlin@gmail.com>
2024-12-27 07:14:47 -0500

    initialized project, added login card

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import LoginCard from "../components/LoginCard";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <LoginCard />
+    </>
+  );
+}
---
+++ app/components/LoginCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginCard = () => {
+  return (
+    <div className="flex items-center justify-center mx-auto h-[50vh] w-[40vh] bg-white border border-red-500 rounded-2xl">
+      <button className="bg-blue-400 text-black p-3 rounded-lg">Login with Google</button>
+    </div>
+  )
+}
+
+export default LoginCard
